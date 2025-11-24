3d1a17b868b3a8dd80353582300ebeb64d7735ee
mohamed-saifi <saifimsc@gmail.com>
2025-11-24 10:41:37 +0100

    Simple Factory using java

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Shape circle = ShapeFactory.getShape("Circle");
+        circle.draw();
+
+        Shape rectangle = ShapeFactory.getShape("Rectangle");
+        rectangle.draw();
+
+    }
+}
